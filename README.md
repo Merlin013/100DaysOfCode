2a6294578225c2d74ed1e7e472b2c0526e2e7c9c
Vishal Petkar <vishpetkar18@gmail.com>
2022-06-16 20:24:25 +0530

    Day 1
    Completed Day 1 tasks.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# 100DaysOfCode
- Contains code learnt each day from the 100 Days of Code course.
+# \#100DaysOfCode
+ Contains code learnt from the 100 Days of Code course(Udemy) and logs my progress from each day.
+
+###Day 1 (16-06-2022) 
+- Learnt about printing techniques.
+- String Concatenation
+- Debugging
+- Input Function 
+- Python Variables
+- Variables naming
+- Band name Generator
---
+++ Section-1/Day-01-Beginner-Working-with-Variables-in-Python-to-Manage-Data/printing-start.py
@@ -0,0 +1,54 @@
+print("Hello world")
+print("Day 1 - Python Print Function\nThe function is declared like this:\nprint('what to print')")
+
+print("Hello" + " Angela")
+
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
+
+name = input("What is your name?")
+print(len(name))
+
+print(len(input("What is your name?")))
+
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+# Write your code below this line 👇
+c = a
+a = b
+
+b = c
+
+
+
+# Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
+
+
+#1. Create a greeting for your program.
+
+print("Hey there! Welcome to the Band Name Generator.")
+
+#2. Ask the user for the city that they grew up in.
+
+city = input("What city did you grow up in? ")
+
+#3. Ask the user for the name of a pet.
+
+pet = input("What is the name of your pet? ")
+
+#4. Combine the name of their city and pet and show them their band name.
+
+print("Your band name could be " + city + " " + pet + "\nEnjoy !!")
+
+
