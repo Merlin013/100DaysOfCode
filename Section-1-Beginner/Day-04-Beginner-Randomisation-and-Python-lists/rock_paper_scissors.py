f4009b420c75b3da3cfd3cae54d9013d0bbf7c31
Vishal Petkar <vishpetkar18@gmail.com>
2022-06-29 22:51:38 +0530

    Updated rock paper scissors program
    Updated rock paper scissors program

--- Section-1-Beginner/Day-04-Beginner-Randomisation-and-Python-lists/rock_paper_scissors.py
+++ Section-1-Beginner/Day-04-Beginner-Randomisation-and-Python-lists/rock_paper_scissors.py
@@ -31,22 +31,23 @@ game_image = [rock, paper, scissors]
 users_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
 
 print("Users Choice")
-print(game_image[users_choice])
-
-comp_choice = random.randint(0, 2)
-
-print("Computers Choice")
-print(game_image[comp_choice])
-
 if users_choice >= 3 or users_choice < 0:
     print("You typed an invalid number, You Lose.  ☠")
-elif users_choice == 0 and comp_choice == 2:
-    print("You win! 🎉")
-elif comp_choice == 0 and users_choice == 2:
-    print("You lose.  ☠")
-elif comp_choice > users_choice:
-    print("You lose. ☠")
-elif users_choice > comp_choice:
-    print("You win!🎉 ")
-elif comp_choice == users_choice:
-    print("It's a draw.")
+else:
+    print(game_image[users_choice])
+
+    comp_choice = random.randint(0, 2)
+
+    print("Computers Choice")
+    print(game_image[comp_choice])
+
+    if users_choice == 0 and comp_choice == 2:
+        print("You win! 🎉")
+    elif comp_choice == 0 and users_choice == 2:
+        print("You lose.  ☠")
+    elif comp_choice > users_choice:
+        print("You lose. ☠")
+    elif users_choice > comp_choice:
+        print("You win!🎉 ")
+    elif comp_choice == users_choice:
+        print("It's a draw.")
