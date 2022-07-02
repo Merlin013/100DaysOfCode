27099f8b97177ec09f0e71c20fb3262c72ff9e91
Vishal Petkar <vishpetkar18@gmail.com>
2022-07-02 18:03:01 +0530

    Updated hangman program
    Updated hangman program

--- Section-1-Beginner/Day-07-Beginner-Hangman/hangman_program.py
+++ Section-1-Beginner/Day-07-Beginner-Hangman/hangman_program.py
@@ -1,16 +1,18 @@
 import random
 word_list = ["aardvark", "baboon", "camel"]
 
-# TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
-
 chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
+display = []
+for _ in range(word_length):
+    display += '_'
+print(display)
 
-# TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
 guess = input("Guess a letter: ").lower()
 
-# TODO-3 - Check if the letter the user guessed (guess) is one of the leters in the chosen_word.
-for letter in chosen_word:
+for position in range(0,word_length):
+    letter = chosen_word[position]
     if letter == guess:
-        print("Right")
-    else:
-        print("Wrong")
+        display[position] = letter
+
+print(display)
