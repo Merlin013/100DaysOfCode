cd56cc3b461137e8b8fc08a21b6c79f979ee4544
Vishal Petkar <vishpetkar18@gmail.com>
2022-07-07 16:38:45 +0530

    Updated secret auction program
    Updated secret auction program

---
+++ Section-1-Beginner/Day-09-Beginner-Dictionaries-nesting-&-secret-auctions/secret_auction_program.py
@@ -0,0 +1,26 @@
+import os
+
+bids = {}
+bidding = True
+
+while bidding:
+    print("Welcome to the secret Auction Program!")
+    name = input("Enter your name: ")
+    bid = int(input("What is your bid?: $"))
+    bids = {name: bid}
+    answer = input("Are there any other bidders? Type 'yes' or 'no'.\n").lower()
+    if answer == "yes":
+        os.system("cls")
+    else:
+        bidding = False
+
+highest_bid = 0
+
+for names in bids:
+    if bids[names] > highest_bid:
+        highest_bid = bids[names]
+        high_name = names
+    else:
+        continue
+
+print("The winner is {} with a bid of ${}.".format(high_name, highest_bid))
