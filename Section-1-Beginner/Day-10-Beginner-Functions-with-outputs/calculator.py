cb96826f79287a19d8738d1fed18a615d2fd42d3
Vishal Petkar <vishpetkar18@gmail.com>
2022-07-08 13:45:36 +0530

    Updated calculator program
    Updated calculator program

--- Section-1-Beginner/Day-10-Beginner-Functions-with-outputs/calculator.py
+++ Section-1-Beginner/Day-10-Beginner-Functions-with-outputs/calculator.py
@@ -1,3 +1,6 @@
+from calculator_art import logo
+
+
 # Add function
 def add(n1, n2):
     return n1 + n2
@@ -24,19 +27,26 @@ symbols = {"+": add,
            "/": div}
 
 
-num1 = int(input("What's the first number?: "))
-for symbol in symbols:
-    print(symbol)
-should_continue = True
-while should_continue:
+def calculator():
+    print(logo)
+    num1 = float(input("What's the first number?: "))
+    for symbol in symbols:
+        print(symbol)
+    should_continue = True
+    while should_continue:
 
-    operation = input("Which operations do you want to perform? ")
-    num2 = int(input("What's the next number?: "))
+        operation = input("Which operations do you want to perform? ")
+        num2 = float(input("What's the next number?: "))
 
-    answer = symbols[operation](num1, num2)
+        answer = symbols[operation](num1, num2)
 
-    print("{} {} {} = {}".format(num1, operation, num2, answer))
-    if input("Type 'y' to continue calculating with {}. ".format(answer)) == "y":
-        num1 = answer
-    else:
-        should_continue = False
+        print("{} {} {} = {}".format(num1, operation, num2, answer))
+        if input("Type 'y' to continue calculating with {}, "
+                 "or type 'n' to start a new calculation: ".format(answer)) == "y":
+            num1 = answer
+        else:
+            should_continue = False
+            calculator()
+
+
+calculator()
