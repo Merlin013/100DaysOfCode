7992dab43121788d3ed80619ccbe7ea1b81eafdd
Vishal Petkar <vishpetkar18@gmail.com>
2022-07-08 13:08:06 +0530

    Created calculator program
    Created calculator program

---
+++ Section-1-Beginner/Day-10-Beginner-Functions-with-outputs/calculator.py
@@ -0,0 +1,28 @@
+# Add function
+def add(n1, n2):
+    return n1 + n2
+
+
+# Sub function
+def sub(n1, n2):
+    return n1 - n2
+
+
+# Multiply function
+def mul(n1, n2):
+    return n1 * n2
+
+
+# Divide function
+def div(n1, n2):
+    return n1 / n2
+
+
+symbols = {"+": add,
+           "-": sub,
+           "*": mul,
+           "/": div}
+
+#function = symbols
+
+
