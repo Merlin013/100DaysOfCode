85b60001729c849788cd9679f775f781d1e19f23
Vishal Petkar <vishpetkar18@gmail.com>
2022-07-10 15:12:06 +0530

    Updated game
    Added a function game_mod()

--- Section-1-Beginner/Day-12-Beginner-Scope-and-number-guessing-game/number_guessing-game.py
+++ Section-1-Beginner/Day-12-Beginner-Scope-and-number-guessing-game/number_guessing-game.py
@@ -1,29 +1,36 @@
 import random
 
+
+def game_mode():
+    difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
+    if difficulty == "easy":
+        guesses = 10
+        return guesses
+    else:
+        guesses = 5
+        return guesses
+
+
 print("Welcome to the Number Guessing game!!\nI'm thinking of a number between 1 and 100.")
-difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
-guessed_correct = False
 number = random.randint(1, 100)
-if difficulty == "easy":
-    guesses = 10
-else:
-    guesses = 5
+guessed_correct = False
+guess_num = game_mode()
 
 while not guessed_correct:
 
-    print("You have {} attempts remaining to guess the number.".format(guesses))
+    print("You have {} attempts remaining to guess the number.".format(guess_num))
     guess = int(input("Make a guess: "))
     if guess == number:
         print("You have guessed correctly!!")
         exit(0)
     elif guess < number:
         print("Your guess is a little low, guess again.")
-        guesses -= 1
+        guess_num -= 1
     elif guess > number:
         print("Your guess is a little high, guess again.")
-        guesses -= 1
+        guess_num -= 1
 
-    if guesses == 0:
-        print("You have {} attempts remaining to guess the number.".format(guesses))
+    if guess_num == 0:
+        print("You have {} attempts remaining to guess the number.".format(guess_num))
         print("You have lost!")
         guessed_correct = True
