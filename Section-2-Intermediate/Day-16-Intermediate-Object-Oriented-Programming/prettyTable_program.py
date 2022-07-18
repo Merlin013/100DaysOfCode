fa3a58cb689822f79d4c5715f568300b23b7fa64
Vishal Petkar <vishpetkar18@gmail.com>
2022-07-18 22:16:02 +0530

    Updated PrettyTable program
    Updated PrettyTable program

--- Section-2-Intermediate/Day-16-Intermediate-Object-Oriented-Programming/prettyTable_program.py
+++ Section-2-Intermediate/Day-16-Intermediate-Object-Oriented-Programming/prettyTable_program.py
@@ -1,7 +1,7 @@
 from prettytable import PrettyTable
 
 table = PrettyTable()
-table.add_column("Pokemon", ["Pikachu", "Squirtle", "Raichu"])
+table.add_column("Pokemon", ["Pikachu", "Squirtle", "Raichu"], align="r")
 table.add_column("Type", ["Electric", "Water", "Electric"])
-
+#table.align = "l"
 print(table)
