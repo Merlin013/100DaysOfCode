a382ee39c7a817a6a53628201263d8de8c6d4f41
Vishal Petkar <vishpetkar18@gmail.com>
2022-07-21 13:05:44 +0530

    Updated turtle_progam file
    Updated turtle_progam file

--- Section-2-Intermediate/Day-18-Intermediate-Turtle-and-GUI-Interface/turtle_program.py
+++ Section-2-Intermediate/Day-18-Intermediate-Turtle-and-GUI-Interface/turtle_program.py
@@ -1,1 +1,31 @@
-from turtle import Turtle
+from turtle import Turtle, Screen
+
+tim = Turtle()
+tim.shape("classic")
+tim.color("red")
+for i in range(4):
+    tim.forward(100)
+    tim.right(90)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
