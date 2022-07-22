04f004c204f9d7619355bd6392671a296eec9474
Vishal Petkar <vishpetkar18@gmail.com>
2022-07-23 00:34:47 +0530

    Update hirst_painting_main.py

--- Section-2-Intermediate/Day-18-Intermediate-Turtle-and-GUI-Interface/hirst_painting_main.py
+++ Section-2-Intermediate/Day-18-Intermediate-Turtle-and-GUI-Interface/hirst_painting_main.py
@@ -1,7 +1,12 @@
 import turtle as turtle_module
 import random
 
+screen = turtle_module.Screen()
+# screen.bgcolor(0, 0, 0)
 tim = turtle_module.Turtle()
+tim.speed("fastest")
+tim.penup()
+tim.hideturtle()
 turtle_module.colormode(255)
 color_list = [(237, 245, 240), (212, 149, 95), (215, 80, 62), (47, 94, 142), (231, 218, 92), (148, 66, 91),
               (22, 27, 40), (155, 73, 60), (122, 167, 195), (40, 22, 29), (39, 19, 15), (209, 70, 89),
@@ -9,7 +14,20 @@ color_list = [(237, 245, 240), (212, 149, 95), (215, 80, 62), (47, 94, 142), (23
               (51, 55, 102), (233, 220, 12), (159, 177, 54), (99, 44, 63), (35, 164, 196), (234, 171, 162),
               (105, 44, 39), (164, 209, 187), (151, 206, 220)]
 
-tim.dot(20, random.choice(color_list))
+tim.setheading(225)
+tim.forward(300)
+tim.setheading(0)
+number_of_dots = 100
 
-screen = turtle_module.Screen()
+for dot_count in range(1, number_of_dots + 1):
+    tim.dot(20, random.choice(color_list))
+    tim.forward(50)
+
+    if dot_count % 10 == 0:
+        tim.setheading(90)
+        tim.forward(50)
+        tim.setheading(180)
+        tim.forward(500)
+        tim.setheading(0)
+
 screen.exitonclick()
