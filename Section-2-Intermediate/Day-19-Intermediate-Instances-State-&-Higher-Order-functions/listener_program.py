bfb626d8d9576a545c6d722adcc8b9020bf1096f
Vishal Petkar <vishpetkar18@gmail.com>
2022-07-25 13:29:45 +0530

    Updated Listener program
    Updated Listener program, with more movement functions and a clear function

--- Section-2-Intermediate/Day-19-Intermediate-Instances-State-&-Higher-Order-functions/listener_program.py
+++ Section-2-Intermediate/Day-19-Intermediate-Instances-State-&-Higher-Order-functions/listener_program.py
@@ -8,6 +8,30 @@ def move_forwards():
     tim.forward(10)
 
 
+def move_backward():
+    tim.backward(10)
+
+
+def turn_left():
+    new_heading = tim.heading() + 10  # This is one method of turning
+    tim.setheading(new_heading)
+
+
+def turn_right():
+    tim.right(10)  # this is another method of turning
+
+
+def clear():
+    tim.clear()
+    tim.penup()
+    tim.home()
+    tim.pendown()
+
+
 screen.listen()
-screen.onkey(key="space", fun=move_forwards)
+screen.onkey(key="w", fun=move_forwards)
+screen.onkey(key="s", fun=move_backward)
+screen.onkey(key="a", fun=turn_left)
+screen.onkey(key="d", fun=turn_right)
+screen.onkey(key="c", fun=clear)
 screen.exitonclick()
