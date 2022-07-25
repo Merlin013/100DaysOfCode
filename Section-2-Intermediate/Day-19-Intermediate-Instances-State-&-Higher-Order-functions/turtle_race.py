9ae5f41837b33107e414336c55ed1a0f3b68f511
Vishal Petkar <vishpetkar18@gmail.com>
2022-07-25 19:40:09 +0530

    Updated Turtle Race program
    Updated Turtle Race program with a title name for the Screen.

--- Section-2-Intermediate/Day-19-Intermediate-Instances-State-&-Higher-Order-functions/turtle_race.py
+++ Section-2-Intermediate/Day-19-Intermediate-Instances-State-&-Higher-Order-functions/turtle_race.py
@@ -1,12 +1,11 @@
 from turtle import Turtle, Screen
 import random
 
-
 screen = Screen()
 is_race_on = False
 # Setting the dimensions of the screen
 screen.setup(width=500, height=400)
-
+screen.title("Turtle Race !")
 # Taking input from user using a popup dialogue box
 user_bet = screen.textinput(title="Make your Bet!", prompt="Which turtle will win the race? 'VIBGYOR' Enter a color: ")
 colors = ["violet", "indigo", "blue", "green", "yellow", "orange", "red"]
@@ -37,6 +36,5 @@ while is_race_on:
                 break
         rand_distance = random.randint(0, 10)
         turtle.forward(rand_distance)
-
 
 screen.exitonclick()
