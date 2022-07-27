a26d2f73b03656e9cdda822a1ffc5dded464e768
Vishal Petkar <vishpetkar18@gmail.com>
2022-07-27 13:14:46 +0530

    Added food function
    Added food function which makes food of different colors spawn randomly across the screen

--- Section-2-Intermediate/Day-20-Intermediate-Build-the-Snake-Game-Part1-Animation&Coordinates/snake_game.py
+++ Section-2-Intermediate/Day-20-Intermediate-Build-the-Snake-Game-Part1-Animation&Coordinates/snake_game.py
@@ -1,6 +1,8 @@
-from turtle import Turtle, Screen
+from turtle import Screen
 import time
 from snake import Snake
+from food import Food
+
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -9,6 +11,7 @@ screen.title("Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
 
 screen.listen()  # to start listening key strokes
 screen.onkey(snake.up, "Up")
@@ -21,5 +24,9 @@ while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
+
+    # Detect collision with food object
+    if snake.head.distance(food) < 15:
+        food.refresh()
 
 screen.exitonclick()
