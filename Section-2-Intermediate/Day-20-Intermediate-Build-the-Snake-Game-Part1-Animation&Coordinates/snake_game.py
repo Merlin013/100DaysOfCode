ecb5ec1d88398fe55f2c67b38accdd50c13aeac5
Vishal Petkar <vishpetkar18@gmail.com>
2022-07-31 13:32:35 +0530

    Update snake_game.py

--- Section-2-Intermediate/Day-20-Intermediate-Build-the-Snake-Game-Part1-Animation&Coordinates/snake_game.py
+++ Section-2-Intermediate/Day-20-Intermediate-Build-the-Snake-Game-Part1-Animation&Coordinates/snake_game.py
@@ -4,11 +4,13 @@ from snake import Snake
 from food import Food
 from scoreboard import Scoreboard
 
+
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
 screen.tracer(0)
+
 
 snake = Snake()
 food = Food()
@@ -31,17 +33,20 @@ while game_is_on:
     if snake.head.distance(food) < 15:
         food.refresh()
         snake.extend()
+
         scoreboard.increase_score()
 
     # Detect collision with wall.
-    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -290:
         game_is_on = False
+
         scoreboard.game_over()
 
     # Detect collision with tail.
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
             game_is_on = False
+
             scoreboard.game_over()
 
 screen.exitonclick()
