ecdca804ff991e336fa4ca59dfb1b2fe3d88a4a7
Vishal Petkar <vishpetkar18@gmail.com>
2022-08-01 13:22:35 +0530

    To detect collision with ball and reset if missed
    To detect collision with ball and reset if missed

--- Section-2-Intermediate/Day-22-Intermediate-Build-Pong-The-Famous-Arcade-Game/pong_game.py
+++ Section-2-Intermediate/Day-22-Intermediate-Build-Pong-The-Famous-Arcade-Game/pong_game.py
@@ -3,7 +3,6 @@ from paddle import Paddle
 from ball import Ball
 import time
 
-
 screen = Screen()
 screen.setup(height=600, width=800)
 screen.title("Arcade Pong Game")
@@ -26,4 +25,23 @@ while game_is_on:
     time.sleep(0.1)
     screen.update()
     ball.move()
+
+    # detect collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        # ball needs to bounce
+        ball.bounce_y()
+
+    # to detect collision with both paddle
+    if (ball.distance(r_paddle) < 50 and ball.xcor() > 320) or (ball.distance(l_paddle) < 50 and ball.xcor() < -320):
+        ball.bounce_x()
+
+    # to detect when the right paddles miss the ball
+    if ball.xcor() > 380:
+        # ball.goto(0, 0)
+        ball.reset_position()
+
+    # to detect when the left paddles miss the ball
+    if ball.xcor() < -380:
+        ball.reset_position()
+
 screen.exitonclick()
