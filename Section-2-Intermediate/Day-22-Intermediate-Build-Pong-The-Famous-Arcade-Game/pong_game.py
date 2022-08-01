8f3d92d6290dd35a76c319da1a9d61874a3439b6
Vishal Petkar <vishpetkar18@gmail.com>
2022-08-01 12:41:23 +0530

    Updated the pong game file
    Updated the pong game file with key bindings for movement and added movement functions

--- Section-2-Intermediate/Day-22-Intermediate-Build-Pong-The-Famous-Arcade-Game/pong_game.py
+++ Section-2-Intermediate/Day-22-Intermediate-Build-Pong-The-Famous-Arcade-Game/pong_game.py
@@ -1,12 +1,8 @@
 from turtle import Screen, Turtle
+from paddle import Paddle
+from ball import Ball
+import time
 
-def go_up():
-    new_y = paddle.ycor() + 20
-    paddle.goto(paddle.xcor(), new_y)
-
-def go_down():
-    new_y = paddle.ycor() - 20
-    paddle.goto(paddle.xcor(), new_y)
 
 screen = Screen()
 screen.setup(height=600, width=800)
@@ -14,24 +10,20 @@ screen.title("Arcade Pong Game")
 screen.bgcolor("black")
 screen.tracer(0)
 
-
-paddle = Turtle("square")
-paddle.color("white")
-paddle.shapesize(stretch_wid=5, stretch_len=1)
-paddle.penup()
-paddle.goto(x=350, y=0)
-
-
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+ball = Ball()
 
 screen.listen()
-screen.onkey(go_up, "Up")
-screen.onkey(go_down, "Down")
-
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
 
 game_is_on = True
 
 while game_is_on:
+    time.sleep(0.1)
     screen.update()
-
-
+    ball.move()
 screen.exitonclick()
