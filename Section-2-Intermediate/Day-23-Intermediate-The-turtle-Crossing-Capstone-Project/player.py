3680ee0de55116f1de0c9fa72e9ff27c8b6009e4
Vishal Petkar <vishpetkar18@gmail.com>
2022-08-02 14:54:06 +0530

    Added finishline and starting line functionality
    Added finishline and starting line functionality

--- Section-2-Intermediate/Day-23-Intermediate-The-turtle-Crossing-Capstone-Project/player.py
+++ Section-2-Intermediate/Day-23-Intermediate-The-turtle-Crossing-Capstone-Project/player.py
@@ -16,12 +16,18 @@ class Player(Turtle):
         self.color("black")
         self.penup()
         self.setheading(90)
-        self.goto(STARTING_POSITION)
+        self.go_to_start()
 
     def move_up(self):
         new_y = self.ycor() + MOVE_DISTANCE
         self.goto(0, new_y)
 
+    def go_to_start(self):
+        self.goto(STARTING_POSITION)
+
     def finish_line(self):
-        if self.ycor() >= 280:
-            self.goto(STARTING_POSITION)
+        if self.ycor() > FINISH_LINE_Y:
+            # self.goto(STARTING_POSITION)
+            return True
+        else:
+            return False
