5e177da11bcb6bdf8b76123ba2ea5afa66363ce2
Vishal Petkar <vishpetkar18@gmail.com>
2022-08-03 14:43:51 +0530

    Added reset function
    Added reset function

--- Section-2-Intermediate/Day-20-Intermediate-Build-the-Snake-Game-Part1-Animation&Coordinates/snake_game.py
+++ Section-2-Intermediate/Day-20-Intermediate-Build-the-Snake-Game-Part1-Animation&Coordinates/snake_game.py
@@ -4,18 +4,15 @@ from snake import Snake
 from food import Food
 from scoreboard import Scoreboard
 
-
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
 screen.tracer(0)
 
-
 snake = Snake()
 food = Food()
 scoreboard = Scoreboard()
-
 
 screen.listen()  # to start listening to keystrokes
 screen.onkey(snake.up, "Up")
@@ -37,16 +34,14 @@ while game_is_on:
         scoreboard.increase_score()
 
     # Detect collision with wall.
-    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -290:
-        game_is_on = False
-
-        scoreboard.game_over()
+    if snake.head.xcor() > 280 or snake.head.xcor() < -285 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        scoreboard.reset()
+        snake.reset()
 
     # Detect collision with tail.
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
-            game_is_on = False
-
-            scoreboard.game_over()
+            scoreboard.reset()
+            snake.reset()
 
 screen.exitonclick()
