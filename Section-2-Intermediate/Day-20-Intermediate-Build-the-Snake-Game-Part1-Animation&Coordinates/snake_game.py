3d8690f3872a3884ef3253c7e3911023e66964a9
Vishal Petkar <vishpetkar18@gmail.com>
2022-08-03 20:45:41 +0530

    Added music and sounds
    Added music and sounds by importing game_sounds.py file

--- Section-2-Intermediate/Day-20-Intermediate-Build-the-Snake-Game-Part1-Animation&Coordinates/snake_game.py
+++ Section-2-Intermediate/Day-20-Intermediate-Build-the-Snake-Game-Part1-Animation&Coordinates/snake_game.py
@@ -3,6 +3,7 @@ import time
 from snake import Snake
 from food import Food
 from scoreboard import Scoreboard
+import game_sounds
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -13,6 +14,7 @@ screen.tracer(0)
 snake = Snake()
 food = Food()
 scoreboard = Scoreboard()
+game_sounds.bg_music()
 
 screen.listen()  # to start listening to keystrokes
 screen.onkey(snake.up, "Up")
@@ -28,19 +30,23 @@ while game_is_on:
 
     # Detect collision with food object
     if snake.head.distance(food) < 15:
+        game_sounds.eaten()
         food.refresh()
+        # game_sounds.food_spawn()
         snake.extend()
 
         scoreboard.increase_score()
 
     # Detect collision with wall.
     if snake.head.xcor() > 280 or snake.head.xcor() < -285 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_sounds.dead()
         scoreboard.reset()
         snake.reset()
 
     # Detect collision with tail.
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
+            game_sounds.dead()
             scoreboard.reset()
             snake.reset()
 
