e859d815326ca6868f85d4b029830409e3170ae3
Vishal Petkar <vishpetkar18@gmail.com>
2022-08-09 11:47:08 +0530

    Update main.py

--- Section-2-Intermediate/Day-25-Intermediate-Working-with-CSV-Data-and-the-Pandas-Library/main.py
+++ Section-2-Intermediate/Day-25-Intermediate-Working-with-CSV-Data-and-the-Pandas-Library/main.py
@@ -16,4 +16,9 @@
 import pandas
 
 data = pandas.read_csv("weather_data.csv")
-print(data)
+# print(data)
+
+monday = data[data.day == "Monday"]
+monday_temp = int(monday.temp)
+monday_temp_F = monday_temp * 9/5 + 32
+print(monday_temp_F)
