d59f3e87acc2e50a608723ae4c177e121dd2ab78
Vishal Petkar <vishpetkar18@gmail.com>
2022-08-09 09:29:45 +0530

    Practice program for Pandas
    Practice program for Pandas

--- Section-2-Intermediate/Day-25-Intermediate-Working-with-CSV-Data-and-the-Pandas-Library/main.py
+++ Section-2-Intermediate/Day-25-Intermediate-Working-with-CSV-Data-and-the-Pandas-Library/main.py
@@ -1,2 +1,19 @@
-with open("weather_data.csv") as file:
-    data = file.readlines()
+# # with open("weather_data.csv") as file:
+# #     data = file.readlines()
+#
+# import csv
+#
+# with open("weather_data.csv") as file:
+#     data = csv.reader(file)
+#     # print(data)
+#     temperatures = []
+#     for row in data:
+#         if row[1] != "temp":
+#             temperatures.append(int(row[1]))
+#
+#     print(temperatures)
+
+import pandas
+
+data = pandas.read_csv("weather_data.csv")
+print(data)
