cb92d337167ad20947a06dcb7d19f682ffe144ca
Vishal Petkar <vishpetkar18@gmail.com>
2022-08-25 22:35:38 +0530

    Made grammar correction for variable name
    Made grammar correction for variable name

--- Section-3-Intermediate+/Day-32-Intermediate+Send-Email-(smtplib)-&-Manage-Dates-(datetime)/Birthday+Wisher+(Day+32)+start/Birthday Wisher (Day 32) start/automated_morning_mail.py
+++ Section-3-Intermediate+/Day-32-Intermediate+Send-Email-(smtplib)-&-Manage-Dates-(datetime)/Birthday+Wisher+(Day+32)+start/Birthday Wisher (Day 32) start/automated_morning_mail.py
@@ -15,7 +15,7 @@ if weekday == 0: # 0 indicates its checking for monday
     with smtplib.SMTP("smtp.gmail.com") as connection:
         connection.starttls()
         connection.login(user=MY_EMAIL, password=MY_PASSWORD)
-        connection.sendmail(from_addr=MY_EMAIL, to_addrs=MY_EMAIl, msg=f"Subject:Monday Motivation\n\n{quote}")
+        connection.sendmail(from_addr=MY_EMAIL, to_addrs=MY_EMAIL, msg=f"Subject:Monday Motivation\n\n{quote}")
 
 
 
